2ffcc2b06aa6e2d3ad952966c48e09a62804722e
lelushi <lelushi@pinduoduo.com>
2023-01-16 11:41:59 +0800

    feat: useTimestamp

---
+++ src/animation/useTimestamp/index.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { useRafFn } from '../../animation/useRafFn';
+import { createTimestamp } from '../../helper';
+
+export interface UseTimestampOptions {
+    offset?: number;
+    immediate?: boolean;
+    callback?: (timestamp: number) => void;
+}
+
+export function useTimestamp(options: UseTimestampOptions = {}) {
+    const { offset = 0, immediate = true, callback } = options;
+    const [timestamp, setTimestamp] = useState(createTimestamp() + offset);
+
+    const update = () => {
+        callback?.(timestamp);
+        setTimestamp(createTimestamp() + offset);
+    };
+
+    useRafFn(update, { immediate });
+
+    return timestamp;
+}
